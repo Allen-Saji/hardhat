a033768940d0b99ea5b0713717f7e0383f41225b
Allen Saji <allensaji04@gmail.com>
2023-05-24 11:03:04 +0530

    sepolia network deployment script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const fs = require("fs");
+const SEPOLIA_PRIVATE_KEY = fs.readFileSync(".sepolia").toString().trim();
+let infuraProjectID = fs.readFileSync(".infura").toString().trim();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/` + infuraProjectID,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: fs.readFileSync(".etherscan").toString().trim(),
+  },
 };
